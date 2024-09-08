544c7ef4a1a4ecb17418e04978e15cc09e983af9
rezpa.snk <rezpa.snk@gmail.com>
2024-09-08 20:11:14 +0200

    refactor schemas

--- backend/db/schemas.py
+++ backend/db/schemas.py
@@ -2,13 +2,35 @@ from pydantic import BaseModel
 from typing import Optional
 
 
-class Trip(BaseModel):
-    id: int
+class TripBase(BaseModel):
     title: str
     text: Optional[str]
 
 
-class Transaction(BaseModel):
+class TripCreate(TripBase):
+    pass
+
+
+class Trip(TripBase):
     id: int
+    
+    class Config:
+        orm_mode = True
+
+
+class TransactionBase(BaseModel):
     title: str
-    cost: float        
+    cost: float      
+    trip_id: int  
+    
+
+class TransactionCreate(TransactionBase):
+    pass
+
+
+class Transaction(TransactionBase):
+    id: int   
+    
+    class Config:
+        orm_mode = True
+         
