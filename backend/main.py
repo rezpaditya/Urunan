ce421a922da13c67af855255987773344dd06f84
rezpa.snk <rezpa.snk@gmail.com>
2024-09-08 11:05:25 +0200

    placeholder post and patch requests

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
 
 app = FastAPI()
+
+
+class Trip(BaseModel):
+    title: str
+    text: Optional[str]
+
+
+class Transaction(BaseModel):
+    title: str
+    cost: float
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
+# TRIPS
 @app.get("/trips")
 async def get_all_trips():
     return {"message": "I see all trips"}
@@ -17,6 +32,17 @@ async def get_trip(id):
     return {"message": f"I see trip with id: {id}"}
 
 
+@app.post("/trip")
+async def create_trip(trip: Trip):
+    return trip
+
+
+@app.patch("/trip")
+async def create_trip(trip: Trip):
+    return {"message": "Trip has been updated."}
+    
+
+# TRANSACTION
 @app.get("/transactions/{trip_id}")
 async def get_all_transactions(trip_id):
     return {"message": f"I see all transaction of trip: {trip_id}"}
@@ -25,3 +51,13 @@ async def get_all_transactions(trip_id):
 @app.get("/transactions/{trip_id}/{transaction_id}")
 async def get_transaction(trip_id, transaction_id):
     return {"message": f"I see transaction with id: {transaction_id} and trip id: {trip_id}"}
+
+
+@app.post("/transaction")
+async def create_trip(transaction: Transaction):
+    return transaction
+
+
+@app.patch("/transaction")
+async def create_trip(transaction: Transaction):
+    return {"message": "Transaction has been updated."}
