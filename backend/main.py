89f8e38a40ba84e7a8316a9629a943521dd7bf12
rezpa.snk <rezpa.snk@gmail.com>
2024-09-17 20:27:15 +0200

    enable CORS

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from sqlalchemy import event
+from fastapi.middleware.cors import CORSMiddleware
 from .db import models, database
 from .db.database import engine
 from .router import trips, transactions
@@ -12,6 +13,18 @@ event.listen(models.User.__table__, 'after_create', database.initialize_table)
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(trips.router)
 app.include_router(transactions.router)
