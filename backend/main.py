6754a22ce9833c4973a3b75f8c90837257c1bcff
rezpa.snk <rezpa.snk@gmail.com>
2024-09-21 09:43:50 +0200

    fix CORS in server error

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,8 @@ app = FastAPI()
 # TODO: define allow origin
 origins = [
     "*",
-    "http://178.128.143.5:8080"
+    "http://127.0.0.1:8080",
+    "http://178.128.143.5:8080",
 ]
 
 app.add_middleware(
