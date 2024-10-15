8b3d7c3f748df7734cbcd9638d5798928cf00e04
rezpa.snk <rezpa.snk@gmail.com>
2024-10-15 21:20:44 +0200

    install tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,vue}"],
+  theme: {
+    extend: {},
+  },
+  darkMode: 'class',
+  plugins: [],
+}
+
