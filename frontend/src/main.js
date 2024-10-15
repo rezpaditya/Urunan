5727c9fda89e39c6da045054fb77113289a9e43e
rezpa.snk <rezpa.snk@gmail.com>
2024-10-15 21:21:07 +0200

    setup tailwind css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
+import './index.css'
 import App from './App.vue'
 import router from './router'
 import { createAuth0 } from '@auth0/auth0-vue'
