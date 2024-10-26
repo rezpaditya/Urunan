2c8a7a94746e261180e9438044f61352e42915e8
rezpa.snk <rezpa.snk@gmail.com>
2024-10-26 14:50:42 +0200

    persist db to local filesystem

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./urunan.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/urunan/urunan.db"
 
 
 engine = create_engine(
